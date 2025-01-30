8d04cd86dbde7d423cefd2b84aaf4d1e2c8ac0ae
li_Shunyang <lishunyang12@163.com>
2025-01-30 17:55:17 +0800

    create my project skeleton with Engineer as chatbot name

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,7 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String partition_line = "-----------------------------------\n";
+        System.out.println(partition_line + "Hello! I'm Engineer \n" + "what can I do dor you? \n" + partition_line +
+                "Bye. Hope to see you again soon! \n" + partition_line);
     }
 }
