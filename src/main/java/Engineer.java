5c251a08ac855f22cd1cbfa46448bf91e97d1947
li_Shunyang <lishunyang12@163.com>
2025-02-15 17:23:45 +0800

    make the happy path prominent in main file

--- src/main/java/Engineer.java
+++ src/main/java/Engineer.java
@@ -1,68 +1,61 @@
 import java.util.Scanner;
 
-
 public class Engineer {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         TaskManager myTaskManager = new TaskManager();
         ChatBotManager myChatBotManager = new ChatBotManager();
         TextDivider myTextDivider = new TextDivider();
-
-        Scanner scanner = new Scanner(System.in);
-        String inputMessage = scanner.nextLine(); // read input
-
         myChatBotManager.sayHello();
-
         boolean isKeepAsking = true;
         do {
+            String inputMessage = scanner.nextLine(); // read input
             String[] words = inputMessage.split(" ");
             String firstWord = words[0];
-            if (!inputMessage.isEmpty()) {
-                switch(firstWord) {
-                case "bye":
-                    myChatBotManager.sayGoodBye();
-                    isKeepAsking = false;
-                    break;
-                case "mark":
-                case "unmark":
-                    if (words.length == 1) {
-                        myChatBotManager.askForID();
-                    } else if (words.length == 2) {
-                        myTaskManager.changeTaskStatus(words);
-                    } else {
-                        myChatBotManager.askForValidTask();
-                    }
-                    break;
-                case "list":
-                    // print out all tasks
-                    myTaskManager.listAllTasks();
-                    break;
-                case "todo":
-                    String inputTaskTodo = words[1] + " " + words[2];
-                    myTaskManager.addTask(inputTaskTodo);
-                    break;
-                case "deadline":
-                    String[] textStringDeadline = myTextDivider.divideDeadline(words);;
-                    String inputTaskDeadline = textStringDeadline[0];
-                    String by = textStringDeadline[1];
-                    myTaskManager.addTask(inputTaskDeadline, by);
-                    break;
-                case "event":
-                    String[] textStringEvent = myTextDivider.divideEvent(words);
-                    String inputTaskEvents = textStringEvent[0];
-                    String from = textStringEvent[1];
-                    String to = textStringEvent[2];
-                    myTaskManager.addTask(inputTaskEvents, from, to);
-                    break;
-                default:
+            if (inputMessage.isEmpty()) {
+                myChatBotManager.askForNonEmptyValue();
+                continue;
+            }
+            switch(firstWord) {
+            case "bye":
+                myChatBotManager.sayGoodBye();
+                isKeepAsking = false;
+                break;
+            case "mark":
+            case "unmark":
+                if (words.length == 1) {
+                    myChatBotManager.askForID();
+                } else if (words.length == 2) {
+                    myTaskManager.changeTaskStatus(words);
+                } else {
                     myChatBotManager.askForValidTask();
                 }
-            } else {
-                myChatBotManager.askForNonEmptyValue();
+                break;
+            case "list":
+                // print out all tasks
+                myTaskManager.listAllTasks();
+                break;
+            case "todo":
+                String inputTaskTodo = words[1] + " " + words[2];
+                myTaskManager.addTask(inputTaskTodo);
+                break;
+            case "deadline":
+                String[] textStringDeadline = myTextDivider.divideDeadline(words);;
+                String inputTaskDeadline = textStringDeadline[0];
+                String by = textStringDeadline[1];
+                myTaskManager.addTask(inputTaskDeadline, by);
+                break;
+            case "event":
+                String[] textStringEvent = myTextDivider.divideEvent(words);
+                String inputTaskEvents = textStringEvent[0];
+                String from = textStringEvent[1];
+                String to = textStringEvent[2];
+                myTaskManager.addTask(inputTaskEvents, from, to);
+                break;
+            default:
+                myChatBotManager.askForValidTask();
             }
-
-        inputMessage = scanner.nextLine();
         } while(isKeepAsking);
-
         scanner.close();  // stop receiving input
     }
 }
